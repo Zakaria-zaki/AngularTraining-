4762f45c952eed17a026a360b6728800d957f95d
zattaoui <03zaki95@gmail.com>
2022-04-15 03:45:16 +0200

    User Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,12 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  item = {id: 1};
 
-  color = 'rgb(255,0,0)'
 
   constructor() {
   }
 
-  changeColor() {
-    const r = Math.floor(Math.random()*255);
-    const g = Math.floor(Math.random()*255);
-    const b = Math.floor(Math.random()*255);
-
-    this.color = `rgb(${r}, ${g}, ${b})`
+  log(event: FocusEvent) {
+    console.log(event);
   }
-
 }
